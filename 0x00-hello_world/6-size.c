ac0520f220548b0344bfa043d369ef50525bd763
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-13 16:33:51 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always (0) (Success) 
+ */
+int main(void)
+{
+	printf("Sze of a char: %lu byte(s)\n", sizeof(char));
+	printf("Sze of a int: %lu byte(s)\n", sizeof(int));
+	printf("Sze of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Sze of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Sze of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
+
+
