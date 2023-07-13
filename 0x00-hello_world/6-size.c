d99d48f4b4365e272e30a4c874b533826a0f1f1a
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-13 16:38:19 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry Point
  *
@@ -7,7 +7,7 @@
 int main(void)
 {
 	printf("Sze of a char: %lu byte(s)\n", sizeof(char));
-	printf("Sze of a int: %lu byte(s)\n", sizeof(int));
+	printf("Sze of an int: %lu byte(s)\n", sizeof(int));
 	printf("Sze of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Sze of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Sze of a float: %lu byte(s)\n", sizeof(float));
