624face8900f916fc469c5cc4eb88e92e0832b2c
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-13 16:42:16 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
  * main - Entry Point
  *
- * Return: Always (0) (Success) 
+ * Return: Always (0)(Success) 
  */
 int main(void)
 {
-	printf("Sze of a char: %lu byte(s)\n", sizeof(char));
-	printf("Sze of an int: %lu byte(s)\n", sizeof(int));
-	printf("Sze of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Sze of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Sze of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 
