a6d8dbd29c1ed87d728ab3d8a82a76f5f5bfbfdc
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-14 05:24:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - generate random numbers to detect -ve or +ve
  *
- * Return: Always 0 (Success)
+ * Return: always (0)
  * 
  */
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
