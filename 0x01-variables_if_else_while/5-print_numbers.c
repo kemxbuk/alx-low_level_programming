b8fb61ece01650c913b1387f9ab6535b733ec51f
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-14 09:57:19 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - assign a random number to the variable n
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+       char num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
