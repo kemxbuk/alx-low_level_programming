9aba0652a039dd531090c10ad8432d781027d145
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-14 09:05:23 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,16 @@ int main(void)
 	char lower = 'a';
 	char upper = 'A';
 
-	while (lower <= 'z')
-	        {
-                putchar(lower);
-                lower++;
-       		}
-	while (upper <= 'Z')
-		{
-		putchar(upper);
-		upper++;
-		}
-        putchar('\n');
-        return (0);
+while (lower <= 'z')
+{
+	putchar(lower);
+        lower++;
+       	}
+while (upper <= 'Z')
+{
+	putchar(upper);
+	upper++;
+	}
+putchar('\n');
+return (0);
 }
