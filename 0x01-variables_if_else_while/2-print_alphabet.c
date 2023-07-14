2c6e79a1ba872f26781db8553fff1a42ffa2e5d7
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-14 08:04:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - assign a random number to the variable n
+ *
+ * Return: always (0)
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
