839454f321c56cd1df3e5d67c93f8a77df49759c
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-18 07:42:14 -0700

    _putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char low = 'a';
+   while ( low <= 'z' )
+   _putchar(low);
+   low++;
+}
+_putchar('\n');
+return (0); 
