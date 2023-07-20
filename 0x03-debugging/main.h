ddbda69aab0188c136f23c7536f185c12cebe0a2
Oluwakemisola Ojo <Oluwakemisola Ojo>
2023-07-19 23:34:02 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_num(int a, int b, int c);
+void print_rem_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
